b6310653236260f2b3f78ea00668b1991d300dde
rlaehrb1 <rlaehrb11@gmail.com>
2021-07-16 16:22:55 +0900

    회원정보관리 프로그램 as

--- src/main/kotlin/memberclass.kt
+++ src/main/kotlin/memberclass.kt
@@ -4,20 +4,20 @@ fun main(){
     println("회원관리프로그램 ver 1.0")
 
     print("회원 수를 입력하세요 : ")
-    val membersCount = readLine()!!.trim().toInt()
+    val membersCount = readLine()!!.trim().toInt()  // 입력을 읽는다, 공백을없앤다, 정수형으로바꾼다.
 
-    val members = Array<member?>(membersCount){null}
+    val members : Array<member> = Array<member?>(membersCount){null} as Array<member> //as Array<member> 형변환
 
     println("== ${membersCount}명의 회원정보를 입력합니다. ==")
 
-    for (i in 0 until membersCount) {
+    for (i in members.indices) {
         val id = i+1
         print("${i+1}번째 회원의 이름 : ")
         val name = readLine()!!.trim()
         print("${i+1}번째 회원의 나이 : ")
         val age = readLine()!!.trim().toInt()
         print("${i+1}번째 회원의 성별 : ")
-        val gender = readLine()!!.trim()[0]
+        val gender = readLine()!!.trim()[0] // [0] 은 Char 형으로한다.
 
         val member = member(id, name, age, gender)
         members[i] = member
@@ -28,7 +28,7 @@ fun main(){
     println("번호/  이름/ 나이 /성별")
 
     for (member in members){
-        println("${member?.id}  / ${member?.name} / ${member?.age}  / ${member?.getGenderKor()}")
+        println("${member.id}  / ${member.name} / ${member.age}  / ${member.getGenderKor()}")
     }
 
 }
